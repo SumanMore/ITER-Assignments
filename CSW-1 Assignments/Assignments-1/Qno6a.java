6be4cf6aa75b75e2303e06b18957dbf1b76fa678
Suman More <67056908+SumanMore@users.noreply.github.com>
2020-11-12 18:08:42 +0530

    Add files via upload

---
+++ CSW-1 Assignments/Assignments-1/Qno6a.java
@@ -0,0 +1,15 @@
+package pack1;
+
+
+
+//public class  Qno6a {
+//	public void show() {
+//		System.out.println("Hello World");
+//	}
+//}
+private class Qno6a
+{
+	public void show() {
+		System.out.println("Hello World");
+	}
+}
