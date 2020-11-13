bd8f5d3dbad571e122bb0462b2ada9ed7ca37e30
Suman More <67056908+SumanMore@users.noreply.github.com>
2020-11-13 17:41:47 +0530

    Update No14.java

--- CSW-1 Assignments/Assignment-3/No14.java
+++ CSW-1 Assignments/Assignment-3/No14.java
@@ -3,32 +3,23 @@
 The first non repeated character in String is: i
 */
 
+
+
 import java.util.*;
 public class No14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc=new Scanner(System.in);
-String str=sc.next();
-str=str.toLowerCase();
-int i,j,c=0;
-
-	for(i=0;i<str.length();i++)
-	{  c=0;
 		
-		for(j=0;j<str.length();j++)
-		{
-			if(str.charAt(j)==str.charAt(i))
-				c++;
+				Scanner sc=new Scanner(System.in);
+				String str=sc.nextLine();
+				 for (int i=0;i<str.length();i++) {
+					 char ch=str.charAt(i);
+					 if(str.indexOf(ch)==str.lastIndexOf(ch)) {
+						 System.out.println(ch);
+						 break;
+					 }
+				}
+			}
 		}
-	if(c==1)
-	{
-		System.out.println(str.charAt(i));
-		break;
-	}
-	
-}
 
-	}
 
-}
