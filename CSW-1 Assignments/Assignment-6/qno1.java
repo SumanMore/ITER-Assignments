522090ff2c3d0b23155b855a39cbc80e2a5f982c
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-03-09 00:04:01 +0530

    Create qno1.java

---
+++ CSW-1 Assignments/Assignment-6/qno1.java
@@ -0,0 +1,10 @@
+
+import java.time.*;
+
+public class qno1 {
+	 public static void main(String[] args)
+	 {
+		 LocalTime todayNewYork=LocalTime.now(ZoneId.of("America/New_York"));
+	       System.out.println("Current Time in IST=" +todayNewYork);
+
+	 }}
